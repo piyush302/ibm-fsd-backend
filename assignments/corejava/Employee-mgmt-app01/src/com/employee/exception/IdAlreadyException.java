5b15e1e166077a06b10ee7cb8ae906359e61b280
PiyushDarshan <PiyushDarshan@DESKTOP-IO404LG>
2019-09-23 11:58:51 +0530

    commit

---
+++ assignments/corejava/Employee-mgmt-app01/src/com/employee/exception/IdAlreadyException.java
@@ -0,0 +1,14 @@
+package com.employee.exception;
+
+public class IdAlreadyException extends Exception {
+	
+	
+	public IdAlreadyException() 
+    { 
+		System.out.println("\n");
+		System.out.println("This already exist");
+		System.out.println("\n");
+    } 
+}
+
+
